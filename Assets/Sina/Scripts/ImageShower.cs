776958686a5153c73c3598f52b0352898e881efe
Frosty5483 <75914780+Frosty5483@users.noreply.github.com>
2026-01-21 12:01:05 +0100

    Für sina

--- Assets/Sina/Scripts/ImageShower.cs
+++ Assets/Sina/Scripts/ImageShower.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ImageShower : MonoBehaviour
+public class ImageShower : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField] private Image image;
     private bool imageShown = false;
+    private bool isOver;
 
     private void Start()
     {
         image.enabled = false;
+    }
+
+    void Update()
+    {
+        HandleExitInput();
     }
 
     public void ChangeState()
@@ -16,5 +23,33 @@ public class ImageShower : MonoBehaviour
         imageShown = !imageShown;
         image.enabled = imageShown;
     }
-    
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        isOver = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        isOver = false;
+    }
+
+    private void HandleExitInput()
+    {
+        if (isOver == false)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                imageShown = !imageShown;
+                image.enabled = imageShown;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            imageShown = !imageShown;
+            image.enabled = imageShown;
+        }
+    }
+
 }
