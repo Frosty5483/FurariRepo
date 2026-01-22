5e68e5a517146107e4074bdad7c2e66747ec79c5
sinakalea <sina.feichtinger08@htl-salzburg.ac.at>
2026-01-22 20:40:00 +0100

    Refactoring 5
    Extract Function

--- Assets/Sina/Scripts/Chat.cs
+++ Assets/Sina/Scripts/Chat.cs
@@ -3,19 +3,16 @@ using UnityEngine;
 public class Chat : MonoBehaviour
 {
     [SerializeField] private GameObject chat;
+    private bool chatActive = false;
    
     void Start()
     {
         chat.SetActive(false);
     }
 
-    public void ShowChat()
+    public void ChangeChat()
     {
-        chat.SetActive(true);
-    }
-
-    public void HideChat() 
-    { 
-        chat.SetActive(false);
+        chatActive = !chatActive;
+        chat.SetActive(chatActive);
     }
 }
