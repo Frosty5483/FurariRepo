a45d2838a09b29e37f4b8588147b71c769e9c8ad
sinakalea <sina.feichtinger08@htl-salzburg.ac.at>
2026-01-22 21:22:32 +0100

    6 Refactoring
    Combine functions into class

--- Assets/Sina/Scripts/Chat.cs
+++ Assets/Sina/Scripts/Chat.cs
@@ -4,15 +4,26 @@ public class Chat : MonoBehaviour
 {
     [SerializeField] private GameObject chat;
     private bool chatActive = false;
-   
+    [SerializeField] private GameObject attachmentList;
+    private bool attachmentListActive;
+
     void Start()
     {
         chat.SetActive(false);
+        attachmentList.SetActive(false);
     }
 
     public void ChangeChat()
     {
         chatActive = !chatActive;
         chat.SetActive(chatActive);
+
+        if (!chatActive) attachmentList.SetActive(false);
+    }
+
+    public void ShowAttachmentList()
+    {
+        attachmentListActive = !attachmentListActive;
+        attachmentList.SetActive(!attachmentListActive);
     }
 }
